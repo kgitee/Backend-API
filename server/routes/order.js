5bfaa32819442cd0e7d673fc6677f5226a79639e
Wilfred Gitee <wilfred.gitee@gmail.com>
2022-02-23 15:57:11 +0300

    Capstone 2 Development - Ecommerce API

--- server/routes/order.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const orderController = require('../controllers/order');
-const router = Router();
-
-router.get('/order/:id',orderController.get_orders);
-router.post('/order/:id',orderController.checkout);
-
-module.exports = router;
